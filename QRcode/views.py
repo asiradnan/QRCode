268d8759ea7c2e12c19a7680283fc1a424b15fb0
Asir <asiradnan23@gmail.com>
2024-06-17 20:57:08 +0600

    Initialized

---
+++ QRcode/views.py
@@ -0,0 +1,25 @@
+import qrcode
+from django.shortcuts import render
+import io
+import base64
+
+
+def qr(request):
+    data=request.POST.get("data")
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(request.POST.get("data"))
+    qr.make(fit=True)
+    img = qr.make_image(back_color=request.POST.get('backcolor'), fill_color=request.POST.get("color"))
+    buffer = io.BytesIO()
+    img.save(buffer, format="PNG")
+    buffer.seek(0)
+    img_base64 = base64.b64encode(buffer.getvalue()).decode('utf-8')
+    return render(request, 'qr.html', {'img': img_base64})
+
+def home(request):
+    return render(request, 'home.html')
