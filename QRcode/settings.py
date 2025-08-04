b8efa8af4ca7a050441a3c81e5469f4882ac3bfe
asiradnan <asiradnan23@gmail.com>
2025-08-04 23:06:25 +0600

    removed whitenoise

--- QRcode/settings.py
+++ QRcode/settings.py
@@ -46,7 +46,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'QRcode.urls'
